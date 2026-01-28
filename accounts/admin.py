a1e6c66965e102aee56352e6a610b6d8ef666089
Vaishali Ajmera <vaishaliajmera11@gmail.com>
2026-01-28 20:18:53 +0530

    added admin config

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, UserOTP
+
+from .models import SystemConfiguration, User, UserOTP
 
 
 @admin.register(User)
@@ -12,7 +12,11 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
         ("Preferences", {"fields": ("preferred_language", "has_set_preferences")}),
     )
-    list_display = BaseUserAdmin.list_display + ("preferred_language", "has_set_preferences")
+    list_display = BaseUserAdmin.list_display + (
+        "preferred_language",
+        "has_set_preferences",
+        "is_staff",
+    )
 
 
 @admin.register(UserOTP)
@@ -28,3 +32,37 @@ class UserOTPAdmin(admin.ModelAdmin):
 
     is_expired.boolean = True
     is_expired.short_description = "Expired?"
+
+
+@admin.register(SystemConfiguration)
+class SystemConfigurationAdmin(admin.ModelAdmin):
+    list_display = (
+        "response_tone",
+        "response_length",
+        "max_daily_questions",
+        "updated_at",
+        "updated_by",
+    )
+    readonly_fields = ("updated_at",)
+    fieldsets = (
+        ("Response Settings", {"fields": ("response_tone", "response_length")}),
+        ("Limits", {"fields": ("max_daily_questions",)}),
+        (
+            "Custom Content",
+            {
+                "fields": ("additional_context", "custom_instructions"),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Metadata",
+            {"fields": ("updated_at", "updated_by"), "classes": ("collapse",)},
+        ),
+    )
+
+    def has_add_permission(self, request):
+        return not SystemConfiguration.objects.exists()
+
+    # def has_delete_permission(self, request, obj=None):
+    #     # Prevent deleting the config
+    #     return False
